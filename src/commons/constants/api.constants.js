4a0bddbc82e484ab08bee4c059c093b517f7eac9
Renu <renu@Renus-MacBook-Air.local>
2020-04-09 17:23:22 +0530

    signup signin implemented

---
+++ src/commons/constants/api.constants.js
@@ -0,0 +1,3 @@
+export const BASE_URL='http://localhost:8000';
+export const USER_URL=`${BASE_URL}/users`;
+export const AUTH_URL=`${BASE_URL}/authentication`;
---
+++ src/components/sign.up/signup.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {Register} from '../../services/user';
+export default class SignUp extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            firstName: '',
+            lastName: '',
+            username: '',
+            password: '',
+            phoneNumber: '',
+            address: ''
+        }
+    }
+    onChangeText = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    signup = () => {
+       Register(this.state).then((success)=>{
+           console.log(success);
+       })
+    }
+    render() {
+        return (
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-md-3"></div>
+                    <div className="col-md-6">
+                        <form>
+                            <div className="form-row">
+                                <div className="form-group col-md-6">
+                                    <label htmlFor="firstName">FirstName</label>
+                                    <input type="text" className="form-control" id="firstName" name="firstName" placeholder="FirstName" onChange={this.onChangeText} />
+                                </div>
+                                <div className="form-group col-md-6">
+                                    <label htmlFor="lastName">LastName</label>
+                                    <input type="text" className="form-control" id="lastName" placeholder="LastName" name="lastName" onChange={this.onChangeText} />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="username">Username</label>
+                                <input type="email" className="form-control" id="username" name="username" placeholder="Username" onChange={this.onChangeText} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <input type="password" className="form-control" id="password" name="password" placeholder="Password" onChange={this.onChangeText} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="phoneNumber">PhoneNumber</label>
+                                <input type="number" className="form-control" id="phoneNumber" name="phoneNumber" placeholder="PhoneNumber" onChange={this.onChangeText} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="address">Address</label>
+                                <textarea type="text" className="form-control" id="address" name="address" placeholder="Address" onChange={this.onChangeText} />
+                            </div>
+                            <button type="button" className="btn btn-primary" onClick={this.signup}>Sign Up</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
